8bebadccf483b8524e4eb136c74561d47480ebd4
lroemmele <lroemmele@gmail.com>
2017-12-30 22:54:03 -0500

    added list items to move up and down list

--- to_do_list_app/to_do/src/App.js
+++ to_do_list_app/to_do/src/App.js
@@ -16,43 +16,51 @@ class App extends Component {
   changeUserInput(input){
     this.setState({
       userInput: input
-    }, ()=> console.log(input))
+    })
   }
 
 //creates a temp array to store changes in list and push new items on the the array, then updates list from listArray
   addToList(input){
-    let listArray = this.state.list;
-
-    listArray.push(input);
+  //  let listArray = this.state.list;
+  //  listArray.push(input);
 
     this.setState({
-      list: listArray,
+      list: [...this.state.list, input],
       userInput: ''
-    })
+    });
   }
 
 //creates temmp empty array, then sets list to emptyArray
   clearList(){
-
     let emptyArray = [];
 
     this.setState({
       list: emptyArray
-    })
+    });
   }
 
 //recieve value and key of selected list item, creates temp array and removes value at that index, then sets list to listarray
   removeItem(val, key){
-    console.log(val+ key);
-    let listArray = this.state.list;
+    //console.log("VALUE " + val + " KEY " + key);
+    let listArray = [...this.state.list];
     listArray.splice(key,1);
 
     this.setState({
-      list: listArray
-    })
+      list: listArray,
+    });
   }
 
+  moveItem(val, key, newKey){
+    //console.log("VALUE " + val + " KEY " + key + " NEWKEY " + direction);
+    let listArray = [...this.state.list];
+    //console.log(listArray);
+    listArray.splice(newKey, 0, listArray.splice(key, 1)[0]);
+    //console.log(listArray);
 
+    this.setState({
+      list: listArray,
+    });
+  }
 
   render() {
     return (
@@ -68,7 +76,8 @@ class App extends Component {
 
           <ul id="listItems">
             {this.state.list.map( (val, key)=>
-              <li>{val} <button onClick={ ()=> this.removeItem(val, key)}>Remove</button></li>)}
+              <li>{val} <button onClick={ ()=> this.removeItem(val, key)}>Remove</button>
+              <button onClick={ ()=> this.moveItem(val, key, key - 1)}>+</button><button onClick={  ()=> this.moveItem(val, key, key + 1) }>-</button></li>)}
           </ul>
           <button onClick={ ()=> this.clearList() }>Clear List</button>
 
